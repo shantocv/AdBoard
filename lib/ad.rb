3cefab47b9658810847281038e388ba81fc21d57
shantocv <shantocv@gmail.com>
2021-05-12 19:00:19 +0530

    cost moved to constant

--- lib/ad.rb
+++ lib/ad.rb
@@ -2,17 +2,18 @@ require 'securerandom'
 
 class Ad
   attr_accessor :ad_id, :ad_type, :ad_rows, :ad_cols, :ad_cost, :origin
-  def initialize(n=1, m=1, type="BW")
-    cost = {
-      "BW": 2,
-      "CL": 8,
-      "FL": 64
-    }
 
+  COST = {
+    "BW": 2,
+    "CL": 8,
+    "FL": 64
+  }
+  
+  def initialize(n=1, m=1, type="BW")
     @ad_type = type
     @ad_rows = n
     @ad_cols = m
-    @ad_cost = cost[type]
+    @ad_cost = COST[type]
     @origin = nil
     @ad_id = SecureRandom.hex(4)
   end
